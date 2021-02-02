81b8c47091a4e5e89a0ec76b4b0d4ec41467118e
NickAlvey0803 <59594631+NickAlvey0803@users.noreply.github.com>
2021-02-01 20:23:54 -0800

    Add files via upload

--- team86_script.js
+++ team86_script.js
@@ -1,42 +1,6 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/team86_index.html'));
-});
-
-app.get('/team86_admin', (req, res) => {
-    res.sendFile('./team86_admin.html', { root: __dirname });
-});
-
-app.get('/team86_findcomp', (req, res) => {
-    res.sendFile('./team86_findcomp.html', { root: __dirname });
-});
-
-app.get('/team86_finduser', (req, res) => {
-    res.sendFile('./team86_finduser.html', { root: __dirname });
-});
-
-app.get('/team86_findvideo', (req, res) => {
-    res.sendFile('./team86_findvideo.html', { root: __dirname });
-});
-
-app.get('/team86_submitvid', (req, res) => {
-    res.sendFile('./team86_submitvid.html', { root: __dirname });
-});
-
-app.get('/team86_newprof', (req, res) => {
-    res.sendFile('./team86_newprof.html', { root: __dirname });
-});
-
-app.get('/team86_myprof', (req, res) => {
-    res.sendFile('./team86_myprof.html', { root: __dirname });
-});
-
-app.get('/team86_viewvideo', (req, res) => {
-    res.sendFile('./team86_viewvideo.html', { root: __dirname });
-});
-
-app.listen(35642);
+app.use(express.static(__dirname + '/public')); //__dir and not _dir
+var port = 35642; // you can use any port
+app.listen(port);
+console.log('server on' + port);
