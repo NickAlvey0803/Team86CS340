e6064426595ef4fae3eb8a16617970d66790dac7
NickAlvey0803 <59594631+NickAlvey0803@users.noreply.github.com>
2021-02-01 20:06:59 -0800

    Add files via upload

--- team86_script.js
+++ team86_script.js
@@ -7,4 +7,36 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/team86_index.html'));
 });
 
+app.get('/team86_admin', (req, res) => {
+    res.sendFile('./team86_admin.html', { root: __dirname });
+});
+
+app.get('/team86_findcomp', (req, res) => {
+    res.sendFile('./team86_findcomp.html', { root: __dirname });
+});
+
+app.get('/team86_finduser', (req, res) => {
+    res.sendFile('./team86_finduser.html', { root: __dirname });
+});
+
+app.get('/team86_findvideo', (req, res) => {
+    res.sendFile('./team86_findvideo.html', { root: __dirname });
+});
+
+app.get('/team86_submitvid', (req, res) => {
+    res.sendFile('./team86_submitvid.html', { root: __dirname });
+});
+
+app.get('/team86_newprof', (req, res) => {
+    res.sendFile('./team86_newprof.html', { root: __dirname });
+});
+
+app.get('/team86_myprof', (req, res) => {
+    res.sendFile('./team86_myprof.html', { root: __dirname });
+});
+
+app.get('/team86_viewvideo', (req, res) => {
+    res.sendFile('./team86_viewvideo.html', { root: __dirname });
+});
+
 app.listen(35642);
